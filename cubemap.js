bd06fb0a0f1b970517fc68feacafefa9811b3453
codazoa <codazoa@protonmail.com>
2022-12-01 23:53:27 -0800

    The beatings will continue until the errors stop

--- cubemap.js
+++ cubemap.js
@@ -11,9 +11,13 @@ class Cubemap {
         this.material = material;
     }
 
-    render( gl, view, perspective ){
+    render_skybox( gl, view, perspective ){
         gl.depthMask( false );
+        gl.cullFace( gl.BACK );
+        gl.enable( gl.CULL_FACE );
         gl.useProgram( this.program );
+
+        view = Cubemap.get_rot_view( view );
 
         set_uniform_matrix4(gl, this.program, "view", view.data );
         set_uniform_matrix4(gl, this.program, "perspective", perspective.data );
@@ -24,6 +28,19 @@ class Cubemap {
         gl.drawArrays( gl.TRIANGLES, 0, 24);
 
         gl.depthMask( true );
+    }
+
+    static get_rot_view( view ){
+        view.data[0]
+
+        let data = [
+            view.data[0], view.data[1], view.data[2],   0,
+            view.data[4], view.data[5], view.data[6],   0,
+            view.data[8], view.data[9], view.data[10],  0,
+            0,            0,            0,              1
+        ];
+
+        return new Mat4( data );
     }
 
     static box_six_sided( gl, program, width, height, depth, material ) {
@@ -83,42 +100,48 @@ class Cubemap {
     }
 
     static loadCubemap( gl, faces ){
+
         let textureID = gl.createTexture();
         gl.bindTexture( gl.TEXTURE_CUBE_MAP, textureID );
-        
+
+        let cubemap_sides = [
+            gl.TEXTURE_CUBE_MAP_POSITIVE_X, gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+            gl.TEXTURE_CUBE_MAP_POSITIVE_Y, gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            gl.TEXTURE_CUBE_MAP_POSITIVE_Z, gl.TEXTURE_CUBE_MAP_NEGATIVE_Z
+        ];
+
         let img = new Array(6);
         let count = 0;
-        for( let i = 0; i < 6; i++ ){
+
+        for( let i = 0; i < faces.length; i++){
             img[i] = new Image();
-            img[i].onload = function() {
-                count++
-                if( count == 6 ){
-                    let cubemap_sides = [
-                        gl.TEXTURE_CUBE_MAP_POSITIVE_X, gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
-                        gl.TEXTURE_CUBE_MAP_POSITIVE_Y, gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
-                        gl.TEXTURE_CUBE_MAP_POSITIVE_Z, gl.TEXTURE_CUBE_MAP_NEGATIVE_Z
-                    ]
-                    
-                    for(let j = 0; j < 6; j++ ){
-                        gl.texImage2D(
-                            cubemap_sides[j],
-                            0, gl.RGBA, 
-                            256, 256, 0,
-                            gl.RGBA, gl.UNSIGNED_BYTE,
-                            img[j]
-                            );
+            gl.texImage2D(
+                cubemap_sides[i],
+                0, gl.RGBA,
+                256, 256, 0,
+                gl.RGBA, gl.UNSIGNED_BYTE,
+                Material.xor_texture()
+            );
+
+            img[i].onload = function () {
+                count++;
+                if( count == 6 ) {
+                    for( let j = 0; j < faces.length; j++ ){
+                        gl.texImage2D( cubemap_sides[j], 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img[j] );
+                        console.log('loaded: ', faces[j]);
                     }
-                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
+                    gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
                 }
-                    gl.generateMipmap ( gl.TEXTURE_CUBE_MAP );
             }
-                
-                img[i].src = faces[i];
+            img[i].src = faces[i];
         }
+
+        
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE); 
 
         return textureID;
     }
